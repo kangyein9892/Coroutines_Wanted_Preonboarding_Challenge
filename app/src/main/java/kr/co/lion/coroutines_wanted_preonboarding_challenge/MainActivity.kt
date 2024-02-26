b899021fb80e9a55e8d9721514f50812cf2ea619
yein <inwon9892@naver.com>
2024-02-27 00:34:19 +0900

    Coroutines Wanted Preonboarding Challenge

---
+++ app/src/main/java/kr/co/lion/coroutines_wanted_preonboarding_challenge/MainActivity.kt
@@ -0,0 +1,14 @@
+package kr.co.lion.coroutines_wanted_preonboarding_challenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
