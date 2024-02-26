e3289a2d8eb89ac54a2facee7aaf3b7ebb15c59f
yein <inwon9892@naver.com>
2024-02-27 01:52:08 +0900

    Cancel

--- app/src/main/java/kr/co/lion/coroutines_wanted_preonboarding_challenge/Asynchronous.kt
+++ app/src/main/java/kr/co/lion/coroutines_wanted_preonboarding_challenge/Asynchronous.kt
@@ -1,9 +1,36 @@
 package kr.co.lion.coroutines_wanted_preonboarding_challenge
 
 import kotlinx.coroutines.*
-import kotlin.system.*
 
 fun main() {
+    runBlocking {
+        println("Weather forecast")
+        println(getWeatherReport())
+        println("Have a good day!")
+    }
+}
+
+suspend fun getWeatherReport() = coroutineScope {
+    val forecast = async { getForecast() }
+    val temperature = async { getTemperature() }
+
+    delay(200)
+    temperature.cancel()
+
+    "${forecast.await()}"
+}
+
+suspend fun getForecast(): String {
+    delay(1000)
+    return "Sunny"
+}
+
+suspend fun getTemperature(): String {
+    delay(1000)
+    return "30\u00b0C"
+}
+
+/*fun main() {
     val time = measureTimeMillis {
         runBlocking {
             runBlocking {
@@ -39,7 +66,7 @@ suspend fun getTemperature(): String {
     delay(500)
     throw AssertionError("Temperature is invalid")
     return "30\u00b0C"
-}
+}*/
 
 /*
 
